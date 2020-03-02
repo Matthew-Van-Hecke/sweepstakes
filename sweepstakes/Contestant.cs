febc09b1ca24a7dd1cd7f40eaad3125c45021568
Matthew VH <vanheckematt@gmail.com>
2020-03-02 11:13:42 -0600

    Added member variables and constructor to Contestant class. However, Contestant constructor repeatedly uses UserInterface.GetUserInputFor method which does not yet exist. Next goal: Write GetUserInputFor method in UserInterface class.

--- sweepstakes/Contestant.cs
+++ sweepstakes/Contestant.cs
@@ -8,5 +8,21 @@ namespace sweepstakes
 {
     class Contestant
     {
+        //Member Variables
+        string firstName;
+        string lastName;
+        string email;
+        string address;
+        int registrationNumber;
+        //Constructor
+        public Contestant()
+        {
+            firstName = UserInterface.GetUserInputFor("first name");
+            lastName = UserInterface.GetUserInputFor("last name");
+            email = UserInterface.GetUserInputFor("email");
+            address = UserInterface.GetUserInputFor("street address");
+            registrationNumber = UserInterface.GetUserInputFor("registration number");
+        }
+        //Member Methods
     }
 }
