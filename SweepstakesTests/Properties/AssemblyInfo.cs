2a6ea1502dbfd3016aad8ec8122a47777bff64f0
Matthew VH <vanheckematt@gmail.com>
2020-03-02 14:00:42 -0600

    Wrote unit tests for ValidateEmail and ValidateInt from UserInterface class. Next goal: Write some unit tests for methods that will be created in Sweepstakes class.

---
+++ SweepstakesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SweepstakesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SweepstakesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("568970f7-0502-4b9b-b321-2ea77559a78b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SweepstakesTests/UserInterfaceTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using sweepstakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SweepstakesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidateEmail_NoAtsOrDots_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "ThisIsNotAnEmailAddress.";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_TextAtDot_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "ThisIsNotAnEmailAddress@.";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_TwoAtsAndADot_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "pat@e@axmple.com";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_OneAtNoDots_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "pat@example";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_OneDotNoAt_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "example.com";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_AtInMiddleAndDotAtEnd_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "pat@examplecom.";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_AtAtBeginningAndDotInMiddle_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "@patexample.com";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_DotBeforeAt_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "pat.example@com";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateEmail_TextAtTextDotText_ReturnTrue()
+        {
+            //Arrange
+            string userInput = "pat@example.com";
+            bool expected = true;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateEmail(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateInt_CompletelyNonIntString_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "pat@example.com";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateInt(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateInt_StringThatContainsIntsAndOtherCharacters_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "Happy50";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateInt(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateInt_SpaceSeparatedInts_ReturnFalse()
+        {
+            //Arrange
+            string userInput = "25 90 65";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateInt(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateInt_IntOnlyInStringFormat_ReturnTrue()
+        {
+            //Arrange
+            string userInput = "30098435809";
+            bool expected = false;
+            bool actual;
+            //Act
+            actual = UserInterface.ValidateInt(userInput);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
